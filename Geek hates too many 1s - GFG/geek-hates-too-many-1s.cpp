cf39a1a42172f5bc53ce42be883c942cd6a61eb0
Naveen Baghel <91206232+itzNaveen@users.noreply.github.com>
2023-03-06 20:32:26 +0530

    Added solution - LeetHub

---
+++ Geek hates too many 1s - GFG/geek-hates-too-many-1s.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+
+  public:
+    int noConseBits(int n) {
+         int num=7<<28;
+        for(int i=28;i>=0;i--)
+        {
+            if(((n&(num>>(28-i)))>>i)==7)
+            n=n^(1<<i);
+        }
+        return n;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int tt;
+    cin >> tt;
+    Solution sol;
+    while (tt--) {
+
+        int n;
+        cin >> n;
+        int ans = sol.noConseBits(n);
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
