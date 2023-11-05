fbdb24c36f9b640d1068005f69ed214099b5f18d
Naveen Baghel <91206232+itzNaveen@users.noreply.github.com>
2023-11-05 08:38:47 +0530

    Added solution - LeetHub

--- Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
+++ Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.cpp
@@ -42,23 +42,38 @@ class Solution
             temp = temp->next;
         }
         
-        // Map the list
-        unordered_map<Node*,Node*>originaltoNew;
+        // Link both the list
         Node *originalNode = head;
         Node *cloneNode = cloneHead;
         while(originalNode != NULL){
-            originaltoNew[originalNode] = cloneNode;
-            originalNode = originalNode->next;
-            cloneNode = cloneNode->next;
+            Node *next = originalNode->next;
+            originalNode->next = cloneNode;
+            originalNode = next;
+            
+            next = cloneNode->next;
+            cloneNode->next = originalNode;
+            cloneNode = next;
         }
         
-        // Link the random pointer
+        // Link the random pointers
+        temp = head;
+        while(temp != NULL){
+            if(temp->next != NULL){
+                temp->next->arb = temp->arb ? temp->arb->next : temp->arb;
+            }
+            temp = temp->next->next;
+        }
+        
+        // Make the list Normal
         originalNode = head;
         cloneNode = cloneHead;
-        while(originalNode != NULL){
-            cloneNode->arb = originaltoNew[originalNode->arb];
-            cloneNode = cloneNode->next;
+        while(originalNode != NULL && cloneNode != NULL){
+            originalNode->next = cloneNode->next;
             originalNode = originalNode->next;
+            if(originalNode != NULL){
+                cloneNode->next = originalNode->next;
+            }
+            cloneNode = cloneNode->next;
         }
         return cloneHead;
     }
